0dacd7225587f0324fe1e24a1090d84c49f859d1
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-03-29 10:56:09 +0300

    added onloading and styles for login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -11,9 +11,11 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       if (isLogin) {
         await signInWithEmailAndPassword(auth, email, password);
@@ -21,35 +23,41 @@ function Login() {
         await createUserWithEmailAndPassword(auth, email, password);
       }
     } catch (error) {
-      console.log("Ошибка аутентификации:", error.message);
+      alert(`Ошибка аутентификации: неправильный логин или пароль`, error);
+    } finally {
+      setLoading(false);
     }
   };
+
   return (
-    
-    <div className={styles.login}>
-      <h2>{isLogin ? "Вход" : "Регистрация"}</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          placeholder="Password"
-        />
-        <button type="submit">
-          {isLogin ? "Войти" : "Зарегистрироваться"}
+    <div className={styles.container}>
+      <div className={styles.login}>
+        <h2>{isLogin ? "Вход" : "Регистрация"}</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="Password"
+          />
+          <button type="submit" disabled={loading}>
+            {loading ? "Загрузка..." : isLogin ? "Войти" : "Зарегистрироваться"}
+          </button>
+        </form>
+        <button type="button" onClick={() => setIsLogin(!isLogin)}>
+          {isLogin
+            ? "Нет аккаунта? Зарегистрируйтесь"
+            : "Есть аккаунт? Войдите!"}
         </button>
-      </form>
-      <button type="submit" onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? "Нет аккаунта? Зарегистрируйтесь" : "Есть аккаунт? Войдите!"}
-      </button>
+      </div>
     </div>
   );
 }
